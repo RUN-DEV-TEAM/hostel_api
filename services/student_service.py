1d7a04267c5a59a004dccf066845ff5643136d74
hameddavid <reganalyst@yahoo.com>
2024-10-15 14:03:12 +0100

    Update on getting correct room for student and logging the user and client that perform the operation

--- services/student_service.py
+++ services/student_service.py
@@ -21,17 +21,20 @@ from services import admin_service
 async def get_student_profile_and_allocate_room_to_the_student_service(mat_no:str,user_meta,  session):
     stud_profile =  external_services.get_student_profile_in_session_given_matno(mat_no)
     curr_session = external_services.get_current_academic_session()
-    if stud_profile[0] and curr_session[0]:
-        stud_obj = stud_profile[1]
-        stud_obj['matric_number'] = mat_no
-        stud_obj['curr_session'] = curr_session[1]
-        stud_obj['medical_attention']= admin_service_helper1.list_of_matric_number_with_health_issue(mat_no)
-        res = await admin_service.first_condition_before_ramdom_room_allocation(stud_obj,user_meta,session)
-        if res[0]:
-            return True,res[1]
-        return False,res[1]
+    if stud_profile[0]:
+        if curr_session[0]:
+            stud_obj = stud_profile[1]
+            stud_obj['matric_number'] = mat_no
+            stud_obj['curr_session'] = curr_session[1]
+            stud_obj['medical_attention']= admin_service_helper1.list_of_matric_number_with_health_issue(mat_no)
+            res = await admin_service.first_condition_before_ramdom_room_allocation(stud_obj,user_meta,session)
+            if res[0]:
+                return True,res[1]
+            return False,res[1]
+        else:
+            return False,curr_session[1]
     else:
-        return False,{"message":"Error getting either student's profile or current seesion"}
+        return False,stud_profile[1]
 
 
 

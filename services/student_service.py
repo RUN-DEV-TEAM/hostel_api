abdec3f31cbaf79abd5d57f338b13bfb8f6e8176
hameddavid <reganalyst@yahoo.com>
2024-10-18 14:33:41 +0100

    getstudent refactoring

--- services/student_service.py
+++ services/student_service.py
@@ -17,24 +17,37 @@ from services import admin_service
 #  
 
 
-
-async def get_student_profile_and_allocate_room_to_the_student_service(mat_no:str,user_meta,  session):
-    stud_profile =  external_services.get_student_profile_in_session_given_matno(mat_no)
-    curr_session = True,'2024/2025' #external_services.get_current_academic_session()
+async def logic_before_get_student_profile_and_allocate_room_to_the_student_service(stud_data:dict,user_meta,  session):
+    
+    stud_profile =  external_services.get_student_profile_in_session_given_matno(stud_data['matric_number'])
     if stud_profile[0]:
-        if curr_session[0]:
-            stud_obj = stud_profile[1]
-            stud_obj['matric_number'] = mat_no
-            stud_obj['curr_session'] = curr_session[1]
-            stud_obj['medical_attention']= admin_service_helper1.list_of_matric_number_with_health_issue(mat_no)
-            res = await admin_service.first_condition_before_ramdom_room_allocation(stud_obj,user_meta,session)
-            if res[0]:
-                return True,res[1]
-            return False,res[1]
-        else:
-            return False,curr_session[1]
+        stud_obj = stud_profile[1]
+        stud_obj['matric_number'] = stud_data['matric_number']
+        stud_obj['curr_session'] = stud_data['curr_session']
+        stud_obj['medical_attention']= admin_service_helper1.list_of_matric_number_with_health_issue(stud_data['matric_number'])
+        res = await admin_service.first_condition_before_ramdom_room_allocation(stud_obj,user_meta,session)
+        if res[0]:
+            return True,res[1]
+        return False,res[1]
     else:
         return False,stud_profile[1]
+    
+
+async def get_student_profile_and_allocate_room_to_the_student_service(mat_no:str,user_meta,  session):
+    
+    curr_session = '2024/2025' #external_services.get_current_academic_session()
+    stud_data = {"matric_number":mat_no, "curr_session":curr_session}
+    check_for_stud_room = await admin_service_helper2.get_student_room_in_session(stud_data,session )
+    if not check_for_stud_room[0]:
+        get_room = await logic_before_get_student_profile_and_allocate_room_to_the_student_service(stud_data,user_meta,session)
+        if get_room[0]:
+            return True, get_room[1]
+        else:
+            return False, get_room[1] 
+    else:
+        return True, check_for_stud_room[1]
+
+  
 
 
 
